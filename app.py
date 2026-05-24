07bea8e9d2fc6d115017e2f94dd78c865456af7c
Muhammad Zubair <mzubair4149@gmail.com>
2026-05-24 10:03:13 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+VERSION = "Version 1.0"
+DEPLOYMENT_STATUS = "Successful"
+
 @app.route('/')
 def home():
-    return "Smart CI/CD Platform"
+    return render_template(
+        'index.html',
+        version=VERSION,
+        status=DEPLOYMENT_STATUS
+    )
 
 @app.route('/health')
 def health():
     return "healthy"
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
